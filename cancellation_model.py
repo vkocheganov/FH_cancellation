224310e5b7bf56d2b463b25873016fc4a688a6aa
Victor Kocheganov <kocheganov@gmail.com>
2021-08-25 11:30:37 +0300

    Add CancellationModel class

---
+++ cancellation_model.py
@@ -0,0 +1,20 @@
+class CancellationModel:
+    def _init_(self):
+        print("initialisation")
+
+    def train(_data):
+        pass
+
+    def dump(_fileName):
+        pass
+
+    def load(_fileName):
+        pass
+
+    def predict(_data):
+        pass
+
+    def score(_data):
+
+
+def load_training_data():
